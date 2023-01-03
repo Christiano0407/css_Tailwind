fa855415ae544b75d3a929b2c24c2bf93ae8b07d
Christiano0407 <placebo0704@gmail.com>
2023-01-03 13:02:24 -0600

    menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-//** Javascript With Tailwind CSS */
+//**TODO ==== === ===  Javascript With Tailwind CSS === === ===== */
+//** === Variables === */
+const mobileMenu = document.querySelector(`#idMobileMenu`);
+const notification = document.querySelector(`#idNotification`);
+const user = document.querySelector(`#user-menu-button`);
+const dropdownNotification = document.querySelector(`#idDropdownPlus`);
+
+//** === Events === */
