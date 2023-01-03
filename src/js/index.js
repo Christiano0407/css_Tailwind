dbe190ac338f4f8edf789de365c787f879f304a4
Christiano0407 <placebo0704@gmail.com>
2023-01-03 15:04:53 -0600

    event clicks

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
 //**TODO ==== === ===  Javascript With Tailwind CSS === === ===== */
 //** === Variables === */
-const mobileMenu = document.querySelector(`#idMobileMenu`);
+const btnMobileMenu = document.querySelector(`#idMobileMenu`);
+const menuMobile = document.querySelector(`#idMenuMobile`);
 const notification = document.querySelector(`#idNotification`);
 const user = document.querySelector(`#user-menu-button`);
 const dropdownNotification = document.querySelector(`#idDropdownPlus`);
+const svgPlus = document.querySelector(`#idSvgPlus`);
+const svg = document.querySelector(`#idSvg`);
 
 //** === Events === */
+
+user.addEventListener(`click`, () => {
+  //console.log('Click');
+  if (dropdownNotification.classList.contains(`hidden`)) {
+    dropdownNotification.classList.add('visible');
+    dropdownNotification.classList.remove('hidden');
+  } else {
+    dropdownNotification.classList.remove('visible');
+    dropdownNotification.classList.add('hidden');
+  }
+  /* 
+  if (dropdownNotification) {
+    dropdownNotification.classList.toggle(`hidden`);
+  } */
+});
+
+btnMobileMenu.addEventListener(`click`, () => {
+  console.log('Mobile');
+
+  if (menuMobile) {
+    menuMobile.classList.toggle(`hidden`);
+    svgPlus.classList.toggle(`hidden`);
+    svg.classList.toggle(`hidden`);
+  }
+});
